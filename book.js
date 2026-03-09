7e774b806bdef64149bec6fc2fb2fb6f398b940a
brianvillain <127741470+brianvillain@users.noreply.github.com>
2026-03-09 10:35:02 +0700

    Update book.js

--- book.js
+++ book.js
@@ -1,4 +1,4 @@
-const allowed = prompt("This page is protected. Enter the password to continue:") === "Iloveyou";
+const allowed = prompt("I changed password. Guess.") === "unfound";
 if (!allowed) {
   document.body.innerHTML = "<h2 style='text-align:center; color: red; margin-top: 20%; font-family: sans-serif;'>Access Denied</h2>";
   throw new Error("Unauthorized access");
